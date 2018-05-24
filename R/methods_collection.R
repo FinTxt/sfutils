48503d29acd7baa28fb0c513507a35b73c1fe5a7
Jasper Ginn <jasperginn@gmail.com>
2018-05-24 16:56:33 +0200

    Add collection class methods

---
+++ R/methods_collection.R
@@ -0,0 +1,11 @@
+# --------------------------------------------------------------------------
+#
+# Methods for the Collection class
+#
+# Standard methods are an extension of generics that already exist in R
+# (e.g. print, plot etc).
+#
+# Specific methods are specific to sfutils (we create the generic in
+# generics.R)
+#
+# --------------------------------------------------------------------------
