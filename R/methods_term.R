47ce94e48ae411c1b2891dc1d5fa304492e5e1ae
Jasper Ginn <jasperginn@gmail.com>
2018-05-24 21:59:18 +0200

    Added terms methods

--- R/methods_term.R
+++ R/methods_term.R
@@ -57,3 +57,54 @@ setMethod("get_context",
             res_unlist
 
           })
+
+#' @rdname get_similar_terms-methods
+#'
+#' @seealso \href{http://documentation.cortical.io/working_with_terms.html}{This} page for more information about contexts and terms
+#'
+#' @export
+#'
+
+setMethod("get_similar_terms",
+          "Term",
+          function(object, contextId = NULL,
+                   posType = NULL, startIndex = 0L) {
+
+            # Get key
+            key <- Sys.getenv("CORTICAL_API_KEY")
+            server <- Sys.getenv("CORTICAL_SERVER")
+            retina <- Sys.getenv("CORTICAL_RETINA")
+
+            # Register
+            conn <- retinasdk$FullClient(key,
+                                         apiServer = server,
+                                         retinaName = retina)
+
+            # Get fingerprint
+            res <- conn$getSimilarTermsForTerm(term(object),
+                                               contextId = contextId,
+                                               posType = posType,
+                                               getFingerprint = TRUE,
+                                               startIndex = startIndex,
+                                               maxResults = 10L)
+
+            # Unlist
+            res_unlist <- lapply(res, function(x) {
+              list(
+                "df" = x$df,
+                "fingerprint" = x$fingerprint$positions,
+                "pos_types" = x$pos_types,
+                "score" = x$score,
+                "term" = x$term
+              )
+            })
+
+            # Assign names
+            res_names <- vapply(res_unlist, function(x) x$term,
+                                "character")
+            names(res_unlist) <- res_names
+
+            # Return
+            res_unlist
+
+          })
